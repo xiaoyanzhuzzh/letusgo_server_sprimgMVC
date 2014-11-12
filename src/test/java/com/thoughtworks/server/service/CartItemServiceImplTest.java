fff9de298ff0bd68d75bac00ddc1c4b3b231703d
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 21:38:06 +0800

    rename to CartItemServiceImplTest

--- src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
+++ src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class CartItemServiceImplTest2 {
+public class CartItemServiceImplTest {
     CartItemDao cartItemDaoImpl;
     CartItemService cartItemServiceImpl;
     CartItem cartItem;
