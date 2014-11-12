c1422bf6083416a8ccce50fc6d9599e5bbc62906
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:50:38 +0800

    finish getCartItemById in service

--- src/main/java/com/thoughtworks/server/service/CartItemServiceImpl.java
+++ src/main/java/com/thoughtworks/server/service/CartItemServiceImpl.java
@@ -1,10 +1,18 @@
 package com.thoughtworks.server.service;
 
+import com.thoughtworks.server.dao.CartItemDao;
 import com.thoughtworks.server.model.CartItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartItemServiceImpl implements CartItemService {
+
+    @Autowired
+    private CartItemDao cartItemDaoImpl;
+
     @Override
     public CartItem getCartItemById(int id) {
-        return null;
+        return cartItemDaoImpl.getCartItemById(id);
     }
 }
