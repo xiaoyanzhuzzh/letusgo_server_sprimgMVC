539ae4168d06486b391614d3652252b14add5330
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 22:40:07 +0800

    modify sql

--- src/main/java/com/thoughtworks/server/dao/CartItemDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/CartItemDaoImpl.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.server.dao;
 
 import com.thoughtworks.server.model.CartItem;
+import com.thoughtworks.server.model.Category;
 import com.thoughtworks.server.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,12 +19,15 @@ public class CartItemDaoImpl implements CartItemDao{
 
     @Override
     public CartItem getCartItemById(int id) {
-        String sql = "SELECT cartItems.*,  items.* FROM cartItems, items WHERE cartItems.cartItem_id = ? AND items.item_id = cartItems.cartItem_itemId";
+        String sql = "SELECT cartItems.*,  items.*, categories.* FROM categories, cartItems, items WHERE cartItems.cartItem_id = ? " +
+                "AND items.item_id = cartItems.cartItem_itemId AND categories.category_id = items.item_categoryId";
         return jdbcTemplate.queryForObject(sql, new RowMapper<CartItem>() {
             @Override
             public CartItem mapRow(ResultSet rs, int i) throws SQLException {
+                Category category = new Category(rs.getInt("category_id"), rs.getString("category_name"));
                 Item item = new Item(rs.getInt("item_id"), rs.getString("item_name"), rs.getDouble("item_price"),
-                        rs.getString("item_unit"), rs.getInt("item_categoryId"));
+                        rs.getString("item_unit"), category);
+
                 return new CartItem(rs.getInt("cartItem_id"), item, rs.getInt("cartItem_num"));
             }
         }, id);
@@ -31,12 +35,15 @@ public class CartItemDaoImpl implements CartItemDao{
 
     @Override
     public List<CartItem> getCartItems() {
-        String sql = "SELECT cartItems.*,  items.* FROM cartItems, items WHERE items.item_id = cartItems.cartItem_itemId";
+        String sql = "SELECT cartItems.*,  items.*, categories.* FROM categories, cartItems, items WHERE " +
+                "items.item_id = cartItems.cartItem_itemId AND categories.category_id = items.item_categoryId";
         return jdbcTemplate.query(sql, new RowMapper<CartItem>() {
             @Override
             public CartItem mapRow(ResultSet rs, int i) throws SQLException {
+                Category category = new Category(rs.getInt("category_id"), rs.getString("category_name"));
                 Item item = new Item(rs.getInt("item_id"), rs.getString("item_name"), rs.getDouble("item_price"),
-                        rs.getString("item_unit"), rs.getInt("item_categoryId"));
+                        rs.getString("item_unit"), category);
+
                 return new CartItem(rs.getInt("cartItem_id"), item, rs.getInt("cartItem_num"));
             }
         });
