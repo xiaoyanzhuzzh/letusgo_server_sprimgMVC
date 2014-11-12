dbdf0beabc89d47b249bb63563634cfa24036644
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:50:14 +0800

    add @Repository tag

--- src/main/java/com/thoughtworks/server/dao/CartItemDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/CartItemDaoImpl.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.server.dao;
 
 import com.thoughtworks.server.model.CartItem;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CartItemDaoImpl implements CartItemDao{
     @Override
     public CartItem getCartItemById(int id) {
