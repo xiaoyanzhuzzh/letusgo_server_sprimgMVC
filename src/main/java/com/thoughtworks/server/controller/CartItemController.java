4a1f2bd432cdeb62bc0d4151501079c498fcb1e9
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:59:04 +0800

    finish getCartItemById method in controller

--- src/main/java/com/thoughtworks/server/controller/CartItemController.java
+++ src/main/java/com/thoughtworks/server/controller/CartItemController.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.server.controller;
 
-/**
- * Created by zhangzhihui on 14-11-12.
- */
+import com.thoughtworks.server.model.CartItem;
+import com.thoughtworks.server.service.CartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class CartItemController {
+    @Autowired
+    private CartItemService cartItemServiceImpl;
+
+    @RequestMapping(value = "/cartItems/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    CartItem getCartItemById(@PathVariable int id){
+        return cartItemServiceImpl.getCartItemById(id);
+    }
 }
