7315169e0e2398265b10bedbfd247ee19605bf22
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 18:46:18 +0800

    rename insertItem to insertCategory

--- src/main/java/com/thoughtworks/server/controller/ItemController.java
+++ src/main/java/com/thoughtworks/server/controller/ItemController.java
@@ -1,5 +1,4 @@
 package com.thoughtworks.server.controller;
-
 import com.thoughtworks.server.model.Item;
 import com.thoughtworks.server.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +17,10 @@ public class ItemController {
         return itemServiceImpl. getItemById(id);
     }
 
+    @RequestMapping(value = "/items", method = RequestMethod.POST)
+    public void insertItem(@RequestBody Item item){
+        itemServiceImpl.insertItem(item);
+    }
     @RequestMapping(value = "/items/{id}", method = RequestMethod.DELETE)
     public void deleteItemById(@PathVariable int id){
         itemServiceImpl. deleteItemById(id);
