48c194f5c0bc8571db60d433025c2afd66cce41a
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:38:27 +0800

    finish cartItem class

--- src/main/java/com/thoughtworks/server/model/CartItem.java
+++ src/main/java/com/thoughtworks/server/model/CartItem.java
@@ -1,7 +1,40 @@
 package com.thoughtworks.server.model;
 
-/**
- * Created by zhangzhihui on 14-11-12.
- */
 public class CartItem {
+    private int id;
+    private int itemId;
+    private int num;
+
+    public CartItem() {
+    }
+
+    public CartItem(int id, int itemId, int num) {
+        this.id = id;
+        this.itemId = itemId;
+        this.num = num;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
 }
