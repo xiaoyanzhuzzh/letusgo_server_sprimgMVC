0faa6c114a117a4ca07bccdff3494606c0932914
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:39:39 +0800

    add @before in test

--- src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
+++ src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.server.service;
 
-/**
- * Created by zhangzhihui on 14-11-12.
- */
+import com.thoughtworks.server.dao.CartItemDao;
+import com.thoughtworks.server.dao.CartItemDaoImpl;
+import com.thoughtworks.server.model.CartItem;
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CartItemServiceImplTest {
+    CartItemDao cartItemDaoImpl;
+
+    @Before
+    public void mock_cartItemDaoImpl(){
+        cartItemDaoImpl = mock(CartItemDaoImpl.class);
+
+        int id = 1;
+        CartItem cartItem = new CartItem(1, 1, 4);
+        when(cartItemDaoImpl.getCartItemById(id)).thenReturn(cartItem);
+    }
 }
