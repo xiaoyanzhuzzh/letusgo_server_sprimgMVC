2a7d9cad4d35f14cc523e3bda7cbfb962cebc59f
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 19:45:44 +0800

    finish can_get_cartItem_by_id test

--- src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
+++ src/test/java/com/thoughtworks/server/service/CartItemServiceImplTest.java
@@ -4,19 +4,32 @@ import com.thoughtworks.server.dao.CartItemDao;
 import com.thoughtworks.server.dao.CartItemDaoImpl;
 import com.thoughtworks.server.model.CartItem;
 import org.junit.Before;
+import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class CartItemServiceImplTest {
     CartItemDao cartItemDaoImpl;
+    CartItemService cartItemServiceImpl;
+    CartItem cartItem;
 
     @Before
     public void mock_cartItemDaoImpl(){
         cartItemDaoImpl = mock(CartItemDaoImpl.class);
 
         int id = 1;
-        CartItem cartItem = new CartItem(1, 1, 4);
+        cartItem = new CartItem(1, 1, 4);
         when(cartItemDaoImpl.getCartItemById(id)).thenReturn(cartItem);
+
+        cartItemServiceImpl = new CartItemServiceImpl();
+    }
+
+    @Test
+    public void can_get_cartItem_by_id(){
+        assertThat(cartItemServiceImpl.getCartItemById(1)).isEqualTo(cartItem);
+        verify(cartItemDaoImpl).getCartItemById(1);
     }
 }
