4442c2f5b92d0f57c33f8239948be054e05a1782
zzh <xiaoyanzhuzzh@sina.com>
2014-11-13 23:04:29 +0800

    finish can_get_category_by_id test codes

--- src/test/java/com/thoughtworks/server/controller/CategoryControllerTest.java
+++ src/test/java/com/thoughtworks/server/controller/CategoryControllerTest.java
@@ -1,7 +1,40 @@
 package com.thoughtworks.server.controller;
 
-/**
- * Created by zhangzhihui on 14-11-13.
- */
+import com.thoughtworks.server.model.Category;
+import com.thoughtworks.server.service.CategoryService;
+import com.thoughtworks.server.service.CategoryServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.ui.ExtendedModelMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 public class CategoryControllerTest {
+    Category category;
+    List<Category> categories = new ArrayList<Category>();
+
+    @Before
+    public void init_category(){
+        category = new Category(1, "水果");
+        categories.add(category);
+    }
+
+    @Test
+    public void can_get_category_by_id(){
+        CategoryService categoryServiceImpl = mock(CategoryServiceImpl.class);
+        int id = 1;
+        when(categoryServiceImpl.getCategoryById(id)).thenReturn(category);
+
+        CategoryController categoryController = new CategoryController();
+        ReflectionTestUtils.setField(categoryController, "categoryServiceImpl", categoryServiceImpl);
+
+        assertEquals("水果", categoryController.getCategoryById(1).getName());
+    }
 }
