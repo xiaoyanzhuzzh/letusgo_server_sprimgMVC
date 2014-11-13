1a2ad4867f233d2a5cab31dcb48eb42ee6cf63b9
zzh <xiaoyanzhuzzh@sina.com>
2014-11-13 19:17:27 +0800

    delete useless imports

--- src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
@@ -3,11 +3,9 @@ package com.thoughtworks.server.dao;
 import com.thoughtworks.server.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
