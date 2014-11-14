4bae7e10e5cdd8e77d58b1f4ea4fd8cf9dce3961
zzh <xiaoyanzhuzzh@sina.com>
2014-11-14 21:59:09 +0800

    finish can_delete_all_cartItems

--- src/test/java/com/thoughtworks/server/controller/PaymentControllerTest.java
+++ src/test/java/com/thoughtworks/server/controller/PaymentControllerTest.java
@@ -1,7 +1,29 @@
 package com.thoughtworks.server.controller;
 
-/**
- * Created by zhangzhihui on 14-11-14.
- */
+import com.thoughtworks.server.service.PaymentService;
+import com.thoughtworks.server.service.PaymentServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class PaymentControllerTest {
+    PaymentService paymentServiceImpl;
+    PaymentController paymentController;
+
+    @Before
+    public void init_PaymentController(){
+        paymentServiceImpl = mock(PaymentServiceImpl.class);
+
+        paymentController = new PaymentController();
+        ReflectionTestUtils.setField(paymentController, "paymentServiceImpl", paymentServiceImpl);
+    }
+
+    @Test
+    public void can_delete_all_cartItems(){
+        paymentController.deleteCartItems();
+        verify(paymentServiceImpl).deleteCartItems();
+    }
 }
