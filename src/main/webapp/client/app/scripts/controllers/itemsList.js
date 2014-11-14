95adaff69d570b2a37603d3376727b9bf9f6a58b
zzh <xiaoyanzhuzzh@sina.com>
2014-11-14 21:05:57 +0800

    delete console.log

--- src/main/webapp/client/app/scripts/controllers/itemsList.js
+++ src/main/webapp/client/app/scripts/controllers/itemsList.js
@@ -19,7 +19,7 @@ angular.module('letusgoApp')
     $scope.addToCartButton = function(item) {
 
       CartItemsService.setCartItems(item);
-
+      
       $scope.$emit('to-parent-cartCount');
     };
 });
