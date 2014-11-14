9b9de420e1bfa15adbf089ba383b475147375c64
zzh <xiaoyanzhuzzh@sina.com>
2014-11-14 22:14:37 +0800

    finish PaymentServiceImpl test

--- src/test/java/com/thoughtworks/server/service/PaymentServiceImplTest.java
+++ src/test/java/com/thoughtworks/server/service/PaymentServiceImplTest.java
@@ -1,7 +1,28 @@
 package com.thoughtworks.server.service;
 
-/**
- * Created by zhangzhihui on 14-11-14.
- */
+import com.thoughtworks.server.dao.CartItemDao;
+import com.thoughtworks.server.dao.CartItemDaoImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class PaymentServiceImplTest {
+    CartItemDao cartItemDaoImpl;
+    PaymentService paymentServiceImpl;
+
+    @Before
+    public void mock_CartItemDaoImpl(){
+        cartItemDaoImpl = mock(CartItemDaoImpl.class);
+        paymentServiceImpl = new PaymentServiceImpl();
+        ReflectionTestUtils.setField(paymentServiceImpl, "cartItemDaoImpl", cartItemDaoImpl);
+    }
+
+    @Test
+    public void can_delete_all_cartItems(){
+        paymentServiceImpl.deleteCartItems();
+        verify(cartItemDaoImpl).deleteCartItems();
+    }
 }
