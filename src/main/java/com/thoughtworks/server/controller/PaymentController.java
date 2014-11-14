f038e2ce70bc410a7c9d9cf604ab171a4d059da9
zzh <xiaoyanzhuzzh@sina.com>
2014-11-14 22:00:53 +0800

    finish deleteCartItems method in controller

--- src/main/java/com/thoughtworks/server/controller/PaymentController.java
+++ src/main/java/com/thoughtworks/server/controller/PaymentController.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.server.controller;
 
-/**
- * Created by zhangzhihui on 14-11-14.
- */
+import com.thoughtworks.server.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentServiceImpl;
+
+    @RequestMapping(value = "/payment", method = RequestMethod.DELETE)
+    public void deleteCartItems() {
+        paymentServiceImpl.deleteCartItems();
+    }
 }
